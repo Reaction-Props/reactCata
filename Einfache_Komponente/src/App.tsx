104724e42d33a3d25a96b5ca3cacc267da57a11d
React-Props <daniel.linker@intellicode-solutions.de>
2024-08-14 20:06:42 +0200

    Ubung 1: Eine einfache typsichere Komponente

--- Einfache_Komponente/src/App.tsx
+++ Einfache_Komponente/src/App.tsx
@@ -1,7 +1,21 @@
-
-
 import './App.css'
 import {Greeting} from "./components/ Greeting.tsx";
+
+//Übung 1: Eine einfache typsichere Komponente
+// Aufgabe:
+// Erstelle eine React-Komponente in TypeScript, die eine einfache Begrüßungsnachricht anzeigt. Die Komponente sollte einen Prop name entgegennehmen,
+// der ein string ist. Wenn der name-prop nicht übergeben wird, sollte ein Standardname "Welt" verwendet werden.
+//
+// Tipps:
+//
+// Verwende Props-Typen zur Definition der Typen der Props.
+// Setze einen Standardwert für den name-prop.
+// Beispiel:
+// tsx
+// Code kopieren
+// // Erwartete Ausgabe
+// // <Greeting name="Max" /> sollte "Hallo, Max!" anzeigen
+// // <Greeting /> sollte "Hallo, Welt!" anzeigen
 
 function App() {
 
